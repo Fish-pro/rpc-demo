c6956a666e03d5ee300ddf8215d193588937af07
chen zechun <zechun.chen@daocloud.io>
2021-02-02 11:16:26 +0800

    first commit

---
+++ rpc/add/internal/logic/addlogic.go
@@ -0,0 +1,41 @@
+package logic
+
+import (
+	"context"
+	"github.com/Fish-pro/rpc-demo/rpc/model"
+
+	"github.com/Fish-pro/rpc-demo/rpc/add/add"
+	"github.com/Fish-pro/rpc-demo/rpc/add/internal/svc"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type AddLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewAddLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddLogic {
+	return &AddLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *AddLogic) Add(in *add.AddReq) (*add.AddResp, error) {
+	// 手动代码开始
+	_, err := l.svcCtx.Model.Insert(model.Book{
+		Book:  in.Book,
+		Price: in.Price,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &add.AddResp{
+		Ok: true,
+	}, nil
+	// 手动代码结束
+}
